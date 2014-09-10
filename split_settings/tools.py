b6aad6a5bb5acbda8b36d14d1abaa98771e92367
Wojciech Banaś <fizista@gmail.com>
2014-09-10 08:55:21 +0200

    pep8

--- split_settings/tools.py
+++ split_settings/tools.py
@@ -5,12 +5,15 @@ import os
 import sys
 import types
 
+
 class optional(str):
+
     """Wrap a file path with this class to mark it as optional.
 
     Optional paths don't raise an IOError if file is not found.
     """
     pass
+
 
 def include(*args, **kwargs):
     """Used for including Django project settings from multiple files.
@@ -42,7 +45,8 @@ def include(*args, **kwargs):
         saved_included_file = scope.get('__included_file__')
         pattern = os.path.join(confpath, conffile)
 
-        # find files per pattern, raise an error if not found (unless file is optional)
+        # find files per pattern, raise an error if not found (unless file is
+        # optional)
         files_to_include = glob.glob(pattern)
         if not files_to_include and not isinstance(conffile, optional):
             raise IOError('No such file: %s' % pattern)
@@ -51,8 +55,10 @@ def include(*args, **kwargs):
             scope['__included_file__'] = included_file
             execfile(included_file, {}, scope)
 
-            # add dummy modules to sys.modules to make runserver autoreload work with settings components
-            modulename = '_split_settings.%s' % conffile[:conffile.rfind('.')].replace('/', '.')
+            # add dummy modules to sys.modules to make runserver autoreload
+            # work with settings components
+            modulename = '_split_settings.%s' % conffile[
+                :conffile.rfind('.')].replace('/', '.')
             module = types.ModuleType(modulename)
             module.__file__ = included_file
             sys.modules[modulename] = module
