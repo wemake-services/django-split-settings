403ac280934ea27b8c3d13ac8f7876a181ba7feb
Brady <bfendres@gmail.com>
2025-11-03 23:05:14 -0800

    revert changes in docs conf. update poetry lock file

--- docs/conf.py
+++ docs/conf.py
@@ -24,21 +24,14 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 def _get_project_meta():
-    import os
-    toml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml'))
-    with open(toml_path, 'rb') as f:
-        data = tomllib.load(f)
-    if 'project' in data:
-        return data['project']
-    elif 'tool' in data and 'poetry' in data['tool']:
-        return data['tool']['poetry']
-    raise KeyError("No [project] or [tool.poetry] section found in pyproject.toml")
+    with open('../pyproject.toml', mode='rb') as pyproject:
+        return tomllib.load(pyproject)['tool']['poetry']
 
 
 pkg_meta = _get_project_meta()
-project = str(pkg_meta.get('name', 'django-split-settings'))
+project = str(pkg_meta['name'])
 # The short X.Y version
-version = str(pkg_meta.get('version', '0.0.1'))
+version = str(pkg_meta['version'])
 # The full version, including alpha/beta/rc tags
 release = version
 
